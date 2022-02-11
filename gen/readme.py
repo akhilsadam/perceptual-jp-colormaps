7644da4836322d335bb1dd7b44676e69ed8c0c62
Akhil Sadam <sadam.akhil@gmail.com>
2022-02-11 12:27:38 -0600

    update readme

--- gen/readme.py
+++ gen/readme.py
@@ -10,7 +10,7 @@ A perceptually uniform colormap generator for Matplotlib equipped with tradition
 Additions are welcomed.  
 
 Why perceptually uniform colormaps? Can you not just use default Matplotlib colormaps?   
-- non-perceptually uniform colormaps induce dangerous artifacts, as seen in this link: !(https://i.stack.imgur.com/JcTDb.png).
+- non-perceptually uniform colormaps induce dangerous artifacts, as seen in ![this link]("https://i.stack.imgur.com/JcTDb.png").
 The left colormap introduces new features to the data.
 - Matplotlib perceptually uniform colormaps are not visually appealing and sometimes lack enough contrast.
 With this package, anyone can design colormaps to fit their visual style.
