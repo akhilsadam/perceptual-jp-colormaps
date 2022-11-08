99e65a7e44c1767f6a994298f50bb4781134f363
Akhil Sadam <sadam.akhil@gmail.com>
2022-11-08 11:26:00 -0600

    [auto] update

--- src/jpcm/__init__.py
+++ src/jpcm/__init__.py
@@ -6,7 +6,7 @@ jpcm | perceptual-jp-colormaps
 A perceptually uniform colormap generator for Matplotlib equipped with traditional-ish Japanese colors to serve as a fixed color palette.
 """
 from matplotlib.colors import ListedColormap as LCM
-import jpcm.core.utils as ul
+import jpcm.utils.utils as utils
 import jpcm.core.core as core
 import jpcm.load.load as load
 import jpcm.core.maps as maps
